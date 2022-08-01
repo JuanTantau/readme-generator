7c41c0c7980fdf7956db02da18257fa30f656dfc
Luisito <luisitomac@Luisitos-MacBook-Pro.local>
2022-07-31 23:12:05 -0400

    Installed packages

---
+++ utils/generateMarkdown.js
@@ -0,0 +1,79 @@
+// function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+const renderLicenseBadge =license =>{ 
+    return (!license.length)?'':
+    `![badge](https://img.shields.io/badge/license-${license}-brightgreen)`;
+  
+  };              
+  
+  //function that returns the license link
+  // If there is no license, return an empty string
+  const renderLicenseLink =license => {
+    return (!license.length)?'':
+    '* [License](#license)';
+  };
+  
+  // function that returns the license section of README
+  // If there is no license, return an empty string
+  const renderLicenseSection =license => {
+    return (!license.length)?'':
+    `${renderLicenseBadge(license)}`;  
+  };
+  
+  
+  // function to generate markdown for README
+  const generateMarkdown = data => {
+    const{projectTitle,license,description, installation, usage, contributing,tests, acknowledgments,questions,userName,}= data;
+    
+    return `
+    # ${projectTitle}
+    
+    ## Description
+  
+    ${description}
+  
+    ## Table of Contents
+  
+    * [Installation](#installation)
+    * [Usage](#usage)
+    ${renderLicenseLink(license)} 
+    * [Contributing](#contributing)
+    * [Tests](#tests)
+    * [Questions](#questions)
+  
+    ## Installation
+  
+    ${installation}
+  
+    ## Usage
+  
+    ${usage}
+  
+    ## License
+  
+    ${renderLicenseSection(license)}
+  
+    ## Contributing
+  
+    ${contributing}
+  
+    ## Tests
+  
+    ${tests}
+  
+    ## Acknowlegements
+  
+    ${acknowledgments}
+  
+    ## Questions
+  
+     Find me on GitHub:(https://github.com/${userName})<br />
+    <br />
+    Email: ${questions}<br /><br />
+    
+    
+   `;
+  }
+  
+  module.exports = generateMarkdown;
+  
